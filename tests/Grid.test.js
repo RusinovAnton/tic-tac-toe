0ab59922420866cfa970d4d3f1f61b0790610094
Rusinov.anton <rusinov.amgrade@gmail.com>
2016-06-16 11:32:20 +0300

    add tests for grid init

--- tests/Grid.test.js
+++ tests/Grid.test.js
@@ -8,45 +8,108 @@ import Grid from '../app/services/grid.service';
 describe('Grid()', ()=>{
 
     it('should throw Error on invalid input', ()=>{
-
-        expect(()=>{ new Grid(null, null).empty() }).to.throw(Error);
-        expect(()=>{ new Grid('3', null).empty() }).to.throw(Error);
-        expect(()=>{ new Grid(undefined, null).empty() }).to.throw(Error);
-        expect(()=>{ new Grid(new Grid(), null).empty() }).to.throw(Error);
-
+        var mockGrid =  new Grid();
+        expect(()=>{
+            mockGrid
+                .init(null, null)
+                .then((success)=>{
+                    if (!success) {
+                        throw new Error(' no success sorry ');
+                    }
+                })
+        }).to.throw(Error);
+        expect(()=>{
+            mockGrid
+                .init('3', null)
+                .then((success)=>{
+                    if (!success) {
+                        throw new Error(' no success sorry ');
+                    }
+                })
+        }).to.throw(Error);
+        expect(()=>{
+            mockGrid
+                .init(undefined, null)
+                .then((success)=>{
+                    if (!success) {
+                        throw new Error(' no success sorry ');
+                    }
+                })
+        }).to.throw(Error);
+        expect(()=>{
+            mockGrid
+                .init(new Grid(), null)
+                .then((success)=>{
+                    if (!success) {
+                        throw new Error(' no success sorry ');
+                    }
+                })
+        }).to.throw(Error);
     });
 
     describe('empty()', ()=>{
         it('should create grid with empty cells', ()=>{
-            let mockGrid = new Grid(3, null).empty();
-
-            assert.equal(isArray(mockGrid), true);
-            assert.equal(mockGrid.length, 3);
-            assert.equal(mockGrid[0].length, 3);
-            assert.equal(mockGrid[0][0], null);
-            assert.equal(mockGrid[1][0], null);
-            assert.equal(mockGrid[2][2], null);
-
+            let cells;
+            let mockGrid = new Grid()
+                .init(3, null)
+                .then((success)=>{
+                    if (success) {
+                        assert.equal(isArray(mockGrid), true);
+                        assert.equal(mockGrid.cells.length, 3);
+                        assert.equal(mockGrid.cells[0].length, 3);
+                        assert.equal(mockGrid.cells[0][0], null);
+                        assert.equal(mockGrid.cells[1][0], null);
+                        assert.equal(mockGrid.cells[2][2], null);
+                    }
+                });
+            // done();
         });
     });
     describe('fromState()', ()=>{
         it('should return grid from state', ()=>{
 
-            let mockState = {
-                size: 3,
-                cells: new Grid(3, null).empty()
-            };
+            let mockStateGrid = new Grid()
+                .init(3, null)
+                .then((success)=>{
 
-            mockState.cells[0][2] = 'item';
-            mockState.cells[1][1] = 'item';
-            mockState.cells[2][0] = 'item';
+                    let mockState = {
+                        size: 3,
+                        grid: void 0
+                    };
 
-            let mockGrid = new Grid(undefined, mockState).fromState();
+                    if (success) {
+                        mockStateGrid.grid.cells[0][2] = 'item';
+                        mockStateGrid.grid.cells[1][1] = 'item';
+                        mockStateGrid.grid.cells[2][0] = 'item';
+                        mockState.grid = mockStateGrid;
+                    }
 
-            assert.equal(mockGrid[0][2], 'item');
-            assert.equal(mockGrid[1][1], 'item');
-            assert.equal(mockGrid[2][0], 'item');
+                    return mockState;
 
+                })
+                .then((mockState)=>{
+                    let mockGrid = new Grid()
+                        .init(3, mockState)
+                        .then((success)=>{
+                            if (success) {
+                                assert.equal(mockGrid.cells[0][2], 'item');
+                                assert.equal(mockGrid.cells[1][1], 'item');
+                                assert.equal(mockGrid.cells[2][0], 'item');
+                            }
+                        });
+                });
         })
     });
+    describe('isEmpty(cell)', ()=>{
+        it('should return true if cell is empty', ()=>{
+            let mockGrid = new Grid()
+                .init(3)
+                .then((success)=>{
+                    if (success) {
+
+                    }
+                });
+        });
+    });
+
 });
